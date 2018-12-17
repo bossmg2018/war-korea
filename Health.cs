f38182329d13645e3b0a9479cc7d17a88808bde7
hansungmin <nimgnusnah@naver.com>
2018-12-17 10:37:39 +0900

    Update Health.cs

--- Health.cs
+++ Health.cs
@@ -5,10 +5,15 @@ using UnityEngine.UI;
 
 public class Health : MonoBehaviour
 {
+<<<<<<< HEAD
     //유비는 천재야
     // Hello, World
     //유비야 전투구현
     //건방진 유비
+=======
+
+
+>>>>>>> parent of 729da3b... Hello, World
 
     public const int maxHealth = 100;
 
