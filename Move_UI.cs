17d7ab5730d482ab31b4e8fd25e47851e2965fca
hansungmin <nimgnusnah@naver.com>
2018-12-19 16:24:07 +0900

    Create Move_UI.cs

---
+++ Move_UI.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Move_UI : MonoBehaviour {
+    public RectTransform RT, MoveTile;
+	// Use this for initialization
+	void Start () {
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        if (Vector3.Distance (RT.position, MoveTile.position) > 1)
+            RT.position = Vector3.Lerp (RT.position, MoveTile.position, 2f * Time.deltaTime);
+        else {
+            RT.position = MoveTile.position;
+            // 이동한 타일의 이벤트 or 몬스터와 전투 실행
+        }
+    }
+}
